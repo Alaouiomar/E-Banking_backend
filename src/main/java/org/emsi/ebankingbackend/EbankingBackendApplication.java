9956af56679d0c26b775d18cbb5547437258946f
alaoui omar <elalaouiomar1998@gmail.com>
2022-06-23 14:24:24 +0100

    Seconde com

--- src/main/java/org/emsi/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/org/emsi/ebankingbackend/EbankingBackendApplication.java
@@ -1,11 +1,19 @@
 package org.emsi.ebankingbackend;
 
+import org.emsi.ebankingbackend.dtos.BankAccountDTO;
+import org.emsi.ebankingbackend.dtos.CurrentBankAccountDTO;
+import org.emsi.ebankingbackend.dtos.CustomerDTO;
+import org.emsi.ebankingbackend.dtos.SavingBankAccountDTO;
 import org.emsi.ebankingbackend.entities.*;
 import org.emsi.ebankingbackend.enums.AccountStatus;
 import org.emsi.ebankingbackend.enums.OperationType;
+import org.emsi.ebankingbackend.execptions.BalanceNotSufficientException;
+import org.emsi.ebankingbackend.execptions.BankAccountNotFoundException;
+import org.emsi.ebankingbackend.execptions.CustomerNotFoundException;
 import org.emsi.ebankingbackend.repositories.AccountOperationRepository;
 import org.emsi.ebankingbackend.repositories.BankAccountRepository;
 import org.emsi.ebankingbackend.repositories.CustomerRepository;
+import org.emsi.ebankingbackend.services.BankAccountService;
 import org.emsi.ebankingbackend.services.BankService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +22,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -26,9 +35,36 @@ public class EbankingBackendApplication {
 
 
     @Bean
-    CommandLineRunner commandLineRunner (BankService bankService) {
+    CommandLineRunner commandLineRunner (BankAccountService bankAccountService) {
         return args -> {
-            bankService.consulter();
+            Stream.of("omar","ali","miryam").forEach(name->{
+                CustomerDTO customer = new CustomerDTO();
+                customer.setName(name);
+                customer.setEmail(name+"@gmail.com");
+                bankAccountService.saveCustomer(customer);
+            });
+            bankAccountService.listCustomers().forEach(customer -> {
+                try {
+                    bankAccountService.saveCurrentBankAccount(Math.random()*90000,9000, customer.getId());
+                    bankAccountService.saveSavingBankAccount(Math.random()*120000, 5.5, customer.getId());
+
+                } catch ( CustomerNotFoundException e) {
+                    e.printStackTrace();
+                }
+            });
+            List<BankAccountDTO> bankAccounts = bankAccountService.bankAccountList();
+            for(BankAccountDTO bankAccount:bankAccounts){
+                for (int i =0; i<10; i++){
+                    String accountId;
+                    if(bankAccount instanceof SavingBankAccountDTO){
+                        accountId= ((SavingBankAccountDTO) bankAccount).getId();
+                    }else{
+                        accountId=((CurrentBankAccountDTO) bankAccount).getId();
+                    }
+                    bankAccountService.credit(accountId, 10000+Math.random()*120000, "credit");
+                    bankAccountService.debit(accountId, 10000+Math.random()*9000, "debit");
+                }
+            }
         };
     }
     //@Bean
---
+++ src/main/java/org/emsi/ebankingbackend/web/CustomerRestController.java
@@ -0,0 +1,43 @@
+package org.emsi.ebankingbackend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.emsi.ebankingbackend.dtos.CustomerDTO;
+import org.emsi.ebankingbackend.entities.Customer;
+import org.emsi.ebankingbackend.execptions.CustomerNotFoundException;
+import org.emsi.ebankingbackend.services.BankAccountService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+
+    @GetMapping("/customers")// pour retourner une liste on utilise getmapping
+    public List<CustomerDTO> customers(){
+        return bankAccountService.listCustomers();
+    }
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
+        return bankAccountService.getCustomer(customerId);
+    }
+    @PostMapping("/customers")// pour sauvegarder un nv customers on utilise postmapping
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+
+    @PutMapping("/customers/{customerId}")// pour modifier on utilse putMapping
+    //pathVariabl pour syncro les id du mapping et id de la methode
+    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO) {
+        customerDTO.setId(customerId);
+       return bankAccountService.updateCustomer(customerDTO); //retourner la liste des customers
+    }
+
+    @DeleteMapping("/customers/{id}")// on utilise deletemapping pour supprimer
+    public void deleteCustomer(@PathVariable Long id){
+        bankAccountService.deleteCustomer(id);
+    }
+}
